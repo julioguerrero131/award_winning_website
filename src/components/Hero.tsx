dec65a80e4a0547f735d02455cec82107be4ee32
julioguerrero131 <judaguer@espol.edu.ec>
2025-04-01 12:27:30 -0500

    add css utilities, keyframes and hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+        <div>
+          <div className="mask-clip-path">
+            <div className="special-font">
+              <b>hola</b>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero

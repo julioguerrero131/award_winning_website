971ba5a8cad12134b9b33ba917aae0bf21f9ccae
julioguerrero131 <judaguer@espol.edu.ec>
2025-04-01 13:43:54 -0500

    hero structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ const Hero = () => {
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
         <div>
-          <div className="mask-clip-path">
-            <div className="special-font">
-              <b>hola</b>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div>
+              Mini Video Player
             </div>
           </div>
         </div>

377a79b766593d73d2dfe2049b0b0691c81552a0
julioguerrero131 <judaguer@espol.edu.ec>
2025-04-02 12:48:09 -0500

    add useStates in Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,35 @@
+import { useRef, useState } from "react";
+
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+    setCurrentIndex((prevIndex) => prevIndex + 1)
+  };
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div>
-              Mini Video Player
+            <div onClick={handleMiniVdClick} className="origin-center">
+              {/* Mini Video Player */}
+              <video 
+                ref={nextVideoRef}
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
   )
-}
+};
 
-export default Hero
+export default Hero;

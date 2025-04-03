5683054948b13fedc9c54e58a310fcb341be8ae1
julioguerrero131 <judaguer@espol.edu.ec>
2025-04-03 13:35:50 -0500

    button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,15 +5,18 @@ interface IButton {
   title: string;
   LeftIcon?: IconType;
   containerClass: string;
-}
+};
 
-const Button = ({ id, title, LeftIcon, containerClass } : IButton) => {
+const Button = ({ id, title, LeftIcon, containerClass }: IButton) => {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
       {LeftIcon && <LeftIcon />}
-      {title}
+      <span className='relative incline-flex overflow-hidden font-general text-xs uppercase'></span>
+      <div>
+        {title}
+      </div>
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;

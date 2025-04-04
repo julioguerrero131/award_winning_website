cdd9ee558af09ad3661c0339fbc585c597803220
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-04-04 17:00:07 -0500

    about section with animated title

---
+++ src/components/AnimatedTitle.tsx
@@ -0,0 +1,10 @@
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Disc<b className="special-font">o</b>ver the world's <br />
+      l<b className="special-font">a</b>rgest shared adventure
+    </div>
+  );
+};
+
+export default AnimatedTitle;

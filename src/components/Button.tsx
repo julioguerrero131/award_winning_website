ac8ee43dda837d91304e5dd81a6813524f5c3a94
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-04-18 09:58:41 -0500

    navbar finished

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,20 @@ interface IButton {
   id: string;
   title: string;
   LeftIcon?: IconType;
+  RightIcon?: IconType;
   containerClass: string;
 };
 
-const Button = ({ id, title, LeftIcon, containerClass }: IButton) => {
+const Button = ({ id, title, LeftIcon, RightIcon, containerClass }: IButton) => {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
       {LeftIcon && <LeftIcon />}
-      <span className='relative incline-flex overflow-hidden font-general text-xs uppercase'></span>
-      <div>
-        {title}
-      </div>
+      <span className='relative incline-flex overflow-hidden font-general text-xs uppercase'>
+        <div>
+          {title}
+        </div>
+      </span>
+      {RightIcon && <RightIcon />}
     </button>
   );
 };

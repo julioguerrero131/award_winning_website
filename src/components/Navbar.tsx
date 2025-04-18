2a61751616fbee37a1737bb0e4577b71d17a5435
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-04-18 10:05:18 -0500

    set music off for development

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -79,7 +79,13 @@ const Navbar = () => {
             </div>
 
             <button className="ml-10 flex items-center space-x-0.5" onClick={toggleAudioIndicator}>
-              <audio ref={audioElementRef} className="hidden" src="/audio/loop.mp3" loop/>
+              <audio 
+                ref={audioElementRef} 
+                className="hidden" 
+                src="/audio/loop.mp3" 
+                loop 
+                autoPlay 
+              />
               {[1, 2, 3, 4].map((bar) => (
                 <div
                   key={bar}

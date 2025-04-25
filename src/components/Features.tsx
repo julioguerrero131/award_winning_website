7080588bf261c36e636cd2a293adf02e4a857787
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-04-25 10:59:59 -0500

    fix styles grid features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -36,7 +36,7 @@ interface IFeature {
 const Features = () => {
   return (
     <section className="bg-black pb-52">
-      <div className="container mx-auto pl-3 pr-3 md:px-10">
+      <div className="container mx-auto px-10 md:px-20">
         <div className="px-5 py-32">
           <p className="font-circular-web text-lg text-blue-50">Into the Metagame Layer</p>
 
@@ -58,7 +58,7 @@ const Features = () => {
         </div>
 
         <div className="grid w-full h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <div className="bento-tilt_1 col-span-2 row-span-2 md:col-span-1">
             <BentoCard
               src="videos/feature-2.mp4"
               title={
@@ -68,7 +68,7 @@ const Features = () => {
             />
           </div>
 
-          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+          <div className="bento-tilt_1 col-span-1 row-span-1">
               <BentoCard
                 src="videos/feature-3.mp4"
                 title={
@@ -78,7 +78,7 @@ const Features = () => {
               />
           </div>
 
-          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          <div className="bento-tilt_1 col-span-1 row-span-1">
               <BentoCard
                 src="videos/feature-4.mp4"
                 title={

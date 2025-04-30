e2ae0a09217beef905f197e6c031ad681e40f4ef
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-04-30 16:59:37 -0500

    autoplay hero video

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,8 +52,7 @@ const Navbar = () => {
       audioElementRef.current?.pause();
     }
   }, [isAudioPlaying]);
-
-
+  
   return (
     <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
       <header className="absolute top-1/2 w-full -translate-y-1/2">
